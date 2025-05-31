11b26d5be826864577a83939a69cc5b2401b3fd4
Philip Soliman <philipsoliman4133@gmail.com>
2025-05-31 20:39:35 +0200

    Added system assembly class

---
+++ code/model/problem.py
@@ -0,0 +1,284 @@
+from datetime import datetime
+from enum import Enum
+
+import matplotlib.pyplot as plt
+import ngsolve as ngs
+import numpy as np
+from fespace import FESpace
+from mesh import BoundaryName, TwoLevelMesh
+
+
+class BoundaryType(Enum):
+    """
+    BoundaryType
+    A class representing the type of boundary condition for a PDE problem.
+    """
+
+    DIRICHLET = "dirichlet"
+    NEUMANN = "neumann"
+    ROBIN = "robin"
+
+
+class Problem:
+    def __init__(self, two_mesh: TwoLevelMesh):
+        """
+        Initialize the problem with the given coefficient function, source function, and boundary conditions.
+
+        Args:
+            coefficient_function (callable): Function representing the coefficient in the PDE.
+            source_function (callable): Function representing the source term in the PDE.
+            boundary_conditions (dict): Dictionary containing boundary conditions for the problem.
+        """
+        self.two_mesh = two_mesh
+        self.num_bcs = len(BoundaryName)
+        self._num_bcs_set = 0
+        self._linear_form = None
+        self._linear_form_set = False
+        self._bilinear_form = None
+        self._bilinear_form_set = False
+        self.boundary_conditions = {}
+        self.boundary_values = {}
+        self.fes = None
+
+    def set_boundary_condition(
+        self,
+        bname: BoundaryName,
+        btype: BoundaryType,
+        value: float | ngs.CoefficientFunction,
+    ):
+        """
+        Set the boundary conditions for the problem.
+
+        Args:
+            conditions (list[BoundaryCondition]): List of boundary conditions.
+        """
+        # store type and boundary name
+        if btype.value in self.boundary_conditions:
+            self.boundary_conditions[btype.value] += f"|{bname.value}"
+        else:
+            self.boundary_conditions[btype.value] = f"{bname.value}"
+
+        self.boundary_values[bname] = {"value": value, "type": btype}
+
+        self._num_bcs_set += 1
+
+    def construct_fespace(self, order: int = 1, discontinuous: bool = False):
+        """
+        Construct the finite element space for the problem.
+
+        Args:
+            order (int, optional): Polynomial order of the finite elements. Defaults to 1.
+            discontinuous (bool, optional): Whether to use discontinuous elements. Defaults to False.
+
+        Returns:
+            FESpace: The finite element space for the problem.
+        """
+        if self.num_bcs_set != self.num_bcs:
+            raise ValueError(
+                f"Boundary conditions not set for all boundaries. "
+                f"Expected {self.num_bcs} but got {len(self.boundary_conditions)}."
+                f"Boundary conditions: {self.boundary_conditions}"
+            )
+        self.fes = FESpace(
+            self.two_mesh,
+            order=order,
+            discontinuous=discontinuous,
+            **self.boundary_conditions,
+        )
+        self._linear_form = ngs.LinearForm(self.fes.fespace)
+        self._bilinear_form = ngs.BilinearForm(self.fes.fespace, symmetric=True)
+
+    def get_trial_and_test_functions(self):
+        """
+        Get the trial and test functions for the finite element space.
+
+        Returns:
+            tuple: A tuple containing the trial and test functions.
+        """
+        if self.fes is None:
+            raise ValueError(
+                "Finite element space has not been constructed yet. Call construct_fespace() first."
+            )
+        u = self.fes.u
+        v = self.fes.v
+        return u, v
+
+    @property
+    def linear_form(self):
+        return self._linear_form
+
+    @linear_form.setter
+    def linear_form(self, lf: ngs.LinearForm):
+        if self._linear_form is None:
+            raise ValueError(
+                "Linear form has not been initialized. Call construct_fespace() first."
+            )
+        if isinstance(self._linear_form, ngs.LinearForm):
+            raise ValueError("Linear form must be set using set_linear_form().")
+        self._linear_form = lf
+
+    def set_linear_form(self, lf: ngs.LinearForm):
+        """
+        Set the linear form for the finite element space.
+
+        Args:
+            lf (ngs.LinearForm): The linear form to set.
+        """
+        self._linear_form_set = True
+        self._linear_form += lf
+
+    @property
+    def bilinear_form(self):
+        return self._bilinear_form
+
+    @bilinear_form.setter
+    def bilinear_form(self, bf: ngs.BilinearForm):
+        if self._bilinear_form is None:
+            raise ValueError(
+                "Bilinear form has not been initialized. Call construct_fespace() first."
+            )
+        if isinstance(self._bilinear_form, ngs.BilinearForm):
+            raise ValueError("Bilinear form must be set using set_bilinear_form().")
+        self._bilinear_form = bf
+
+    def set_bilinear_form(self, bf: ngs.BilinearForm):
+        """
+        Set the bilinear form for the finite element space.
+
+        Args:
+            bf (ngs.FESpace.BilinearForm): The bilinear form to set.
+        """
+        self._bilinear_form_set = True
+        self._bilinear_form += bf
+
+    def assemble(self):
+        """
+        Assemble the linear and bilinear forms.
+        """
+        if not self._linear_form_set:
+            raise ValueError(
+                "Linear form has not been set. Call set_linear_form() first."
+            )
+        if not self._bilinear_form_set:
+            raise ValueError(
+                "Bilinear form has not been set. Call set_bilinear_form() first."
+            )
+
+        if self._linear_form is None or self._bilinear_form is None:
+            raise ValueError(
+                "Linear or bilinear form has not been initialized. Call construct_fespace() first."
+            )
+        if self.fes is None:
+            raise ValueError(
+                "Finite element space has not been constructed yet. Call construct_fespace() first."
+            )
+        u = ngs.GridFunction(self.fes.fespace)
+        for bname, data in self.boundary_values.items():
+            value = data["value"]
+            btype = data["type"]
+            if btype == BoundaryType.DIRICHLET:
+                print(f"Setting Dirichlet condition on {bname.value} with value {value}")
+                u.Set(value, definedon=bname.value)
+            else:
+                raise ValueError(
+                    f"Unsupported boundary condition type: {btype}. "
+                    "Only Dirichlet conditions are supported."
+                )
+        b = self._linear_form.Assemble()
+        A = self._bilinear_form.Assemble()
+
+        return u, b, A
+
+    def direct_ngs_solve(self, u: ngs.Vector, b: ngs.Vector, A: ngs.Matrix):
+        """
+        Solve the system of equations directly.
+
+        Args:
+            gfu (ngs.GridFunction): The grid function to store the solution.
+            system (ngs.Matrix): The system matrix.
+            load (ngs.Vector): The load vector.
+        """
+        if self.fes is None:
+            raise ValueError(
+                "Finite element space has not been constructed yet. Call construct_fespace() first."
+            )
+        res = b.vec.CreateVector()
+        res.data = b.vec - A.mat * u.vec
+        u.vec.data += A.mat.Inverse(self.fes.fespace.FreeDofs(), inverse="sparsecholesky") * res
+
+    def save_ngs_solution(self, sol: ngs.GridFunction, problem_name: str):
+        """
+        Save the grid function solution to a file.
+
+        Args:
+            sol (ngs.GridFunction): The grid function containing the solution.
+            problem_name (str): The name of the problem (used for the filename).
+        """
+        if self.fes is None:
+            raise ValueError(
+                "Finite element space has not been constructed yet. Call construct_fespace() first."
+            )
+
+        # Get current date and time as a string
+        now_str = datetime.now().strftime("%Y%m%d_%Hh%Mm%Ss")
+        fn = f"solution_{now_str}_{problem_name}"
+        tlm_dir = self.two_mesh.save_dir
+        vtk = ngs.VTKOutput(
+            self.two_mesh.fine_mesh,
+            coefs=[sol],
+            names=[f"{problem_name}_solution"],
+            filename=str(tlm_dir / fn),
+            subdivision=2,
+        )
+        vtk.Do()
+
+    @property
+    def num_bcs_set(self):
+        """Number of boundary conditions set."""
+        return self._num_bcs_set
+
+
+if __name__ == "__main__":
+    # load mesh
+    lx, ly = 1.0, 1.0
+    coarse_mesh_size = 0.15
+    refinement_levels = 2
+    layers = 2
+    two_mesh = TwoLevelMesh.load(
+        lx=lx,
+        ly=ly,
+        coarse_mesh_size=coarse_mesh_size,
+        refinement_levels=refinement_levels,
+        layers=layers,
+    )
+
+    # construct finite element space
+    problem = Problem(two_mesh)
+
+    # set boundary conditions
+    problem.set_boundary_condition(BoundaryName.LEFT, BoundaryType.DIRICHLET, 1.0)
+    problem.set_boundary_condition(BoundaryName.RIGHT, BoundaryType.DIRICHLET, 0.0)
+    problem.set_boundary_condition(BoundaryName.BOTTOM, BoundaryType.DIRICHLET, 0.0)
+    problem.set_boundary_condition(BoundaryName.TOP, BoundaryType.DIRICHLET, 0.0)
+
+    # construct finite element space
+    problem.construct_fespace(order=1, discontinuous=False)
+
+    # get trial and test functions
+    u_h, v_h = problem.get_trial_and_test_functions()
+
+    # construct bilinear and linear forms
+    problem.set_bilinear_form(ngs.grad(u_h) * ngs.grad(v_h) * ngs.dx)
+    problem.set_linear_form(
+        32 * (ngs.y * (1 - ngs.y) + ngs.x * (1 - ngs.x)) * v_h * ngs.dx
+    )
+
+    # assemble the forms
+    u, b, A = problem.assemble()
+    print(len(u.vec.data))
+
+    # direct solve
+    problem.direct_ngs_solve(u, b, A)
+
+    # save the solution
+    # problem.save_ngs_solution(u, "poisson")
