fa1ed96024a9d88a09016ec5af379ad9c84a6f8f
Philip Soliman <philipsoliman4133@gmail.com>
2025-06-02 02:50:44 +0200

    Cleaned up coarse spaces

--- code/model/coarse_space.py
+++ code/model/coarse_space.py
@@ -1,38 +1,32 @@
 import numpy as np
 import scipy.sparse as sp
 from fespace import FESpace
+from mesh import TwoLevelMesh
 
 
 class CoarseSpace(object):
+    def __init__(self, A: sp.csr_matrix, fespace: FESpace, two_mesh: TwoLevelMesh):
+        self.fespace = fespace
+        self.A = A
+        self.two_mesh = two_mesh
+
     def apply(self, x: np.ndarray):
         raise NotImplementedError("This method should be implemented by subclasses.")
 
 
 class AMSCoarseSpace(CoarseSpace):
-    def __init__(self, fespace: FESpace, A: sp.csr_matrix):
-        self.fespace = fespace
-        self.A = A
-
     def apply(self, x: np.ndarray):
         # Implement the AMS coarse space application logic here
         raise NotImplementedError("AMS coarse space application not implemented.")
 
 
 class GDSWCoarseSpace(CoarseSpace):
-    def __init__(self, fespace: FESpace, A: sp.csr_matrix):
-        self.fespace = fespace
-        self.A = A
-
-    def apply(self, x):
+    def apply(self, x: np.ndarray):
         # Implement the GDSW coarse space application logic here
         raise NotImplementedError("GDSW coarse space application not implemented.")
 
 
 class RGDSWCoarseSpace(CoarseSpace):
-    def __init__(self, fespace: FESpace, A: sp.csr_matrix):
-        self.fespace = fespace
-        self.A = A
-
     def apply(self, x: np.ndarray):
         # Implement the RGDSW coarse space application logic here
         raise NotImplementedError("RGDSW coarse space application not implemented.")
