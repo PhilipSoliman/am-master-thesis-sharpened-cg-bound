36af60655ea6b8dd7359c29ec73ce37105737bd4
psoliman <p.m.soliman@student.tudelft.nl>
2025-06-26 19:53:50 +0200

    Default logging level, allow duplicate OMP instances and initialize GPU interface

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,1 +1,13 @@
+import os
+
 from lib.logger import LOGGER, PROGRESS
+
+from .gpu_interface import GPUInterface
+
+os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
+
+# set default logging level for imported lib
+LOGGER.setLevel("INFO")
+
+# get GPU interface
+gpu_interface = GPUInterface()
