0e4fc0dbc03d69f3885a4c13935df4494910bfe4
psoliman <p.m.soliman@student.tudelft.nl>
2025-06-27 02:06:02 +0200

    Removed default logging level

--- lib/__init__.py
+++ lib/__init__.py
@@ -6,8 +6,5 @@ from .gpu_interface import GPUInterface
 
 os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 
-# set default logging level for imported lib
-LOGGER.setLevel("INFO")
-
 # get GPU interface
 gpu_interface = GPUInterface()
