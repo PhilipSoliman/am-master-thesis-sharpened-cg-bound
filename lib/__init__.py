1226d9e9500e6cacce19405b224464abbc1a3712
psoliman <p.m.soliman@student.tudelft.nl>
2025-07-01 13:59:59 +0200

    Removed default initialization of GPU interface

--- lib/__init__.py
+++ lib/__init__.py
@@ -2,9 +2,5 @@ import os
 
 from lib.logger import LOGGER, PROGRESS
 
-from .gpu_interface import GPUInterface
 
 os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
-
-# get GPU interface
-gpu_interface = GPUInterface()
