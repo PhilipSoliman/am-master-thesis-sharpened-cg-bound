b7d71e278f9d731d566e615a3c31ae7c9ee0fb2d
psoliman <p.m.soliman@student.tudelft.nl>
2025-07-04 17:32:30 +0200

    Implemented automatic k-means clustering of eigenspectra

--- lib/__init__.py
+++ lib/__init__.py
@@ -4,3 +4,4 @@ from lib.logger import LOGGER, PROGRESS
 
 
 os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
+os.environ["OMP_NUM_THREADS"] = "1"
--- lib/eigenvalues/__init__.py
+++ lib/eigenvalues/__init__.py
@@ -1,3 +1,3 @@
-from .eigenvalues import eigs
+from .eigenvalues import eigs, split_spectrum_into_clusters
 
-__all__ = ["eigs"]
+__all__ = ["eigs", "split_spectrum_into_clusters"]
