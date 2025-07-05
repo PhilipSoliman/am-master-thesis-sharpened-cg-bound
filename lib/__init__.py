2b0e7e71ce79b234503dcbbc2f58e1c4b4c392e0
psoliman <p.m.soliman@student.tudelft.nl>
2025-07-05 17:16:21 +0200

    Refactor

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,25 +1,23 @@
+# NOTE: Before anything, set environement variables for torch and scikit-learn to avoid issues with multiple threads
 import os
+
+os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
+os.environ["OMP_NUM_THREADS"] = "1"
 
 from lib.logger import LOGGER, PROGRESS
 
 try:
     # look for command line arguments to set the log level
-    from lib.utils import get_cli_args
+    from lib.cli import CLI_ARGS
 
-    args = get_cli_args()
-    if hasattr(args, "loglvl"):
-        LOGGER.setLevel(args.loglvl)
-    if hasattr(args, "show_progress") and args.show_progress:
+    if hasattr(CLI_ARGS, "loglvl"):
+        LOGGER.setLevel(CLI_ARGS.loglvl)
+    if hasattr(CLI_ARGS, "show_progress") and CLI_ARGS.show_progress:
         LOGGER.debug("Showing progress bar (CLI argument)")
         PROGRESS.show()
     else:
         LOGGER.debug("Hiding progress bar (CLI argument)")
         PROGRESS.hide()
 except Exception as e:
-    LOGGER.exception(
-        f"Failed to set log level or progress bar visibility from CLI arguments. Using default settings. {e}"
-    )
+    LOGGER.exception(f"Failed to get CLI arguments: {e}")
     pass
-
-os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
-os.environ["OMP_NUM_THREADS"] = "1"
