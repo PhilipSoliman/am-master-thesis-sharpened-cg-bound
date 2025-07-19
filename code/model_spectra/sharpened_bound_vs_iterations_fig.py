41873e9d8330c0ebc8b3840d1b25b70464621cc7
psoliman <p.m.soliman@student.tudelft.nl>
2025-07-19 20:27:48 +0200

    Modularized plotting

--- code/model_spectra/sharpened_bound_vs_iterations_fig.py
+++ code/model_spectra/sharpened_bound_vs_iterations_fig.py
@@ -18,7 +18,7 @@ from hcmsfem.plot_utils import save_latex_figure, set_mpl_cycler, set_mpl_style
 from hcmsfem.solvers import mixed_sharpened_cg_iteration_bound
 
 PLOT_MESHES = [DefaultQuadMeshParams.Nc8, DefaultQuadMeshParams.Nc64]
-PLOT_BOUNDS = False
+SHOW_BOUNDS = False
 
 # set matplotlib style & cycler
 set_mpl_style()
@@ -38,7 +38,7 @@ MOVING_AVG_WINDOW = 10
 
 
 def plot_sharpened_bound_vs_iterations(
-    preconditioner, meshes=MESHES, coef_funcs=COEF_FUNCS, plot_bounds=True
+    preconditioner, meshes=MESHES, coef_funcs=COEF_FUNCS, show_bounds=True
 ) -> tuple[plt.Figure, str]:
     # get preconditioner class and coarse space class
     preconditioner_cls, coarse_space_cls = preconditioner
@@ -52,7 +52,7 @@ def plot_sharpened_bound_vs_iterations(
         len(meshes),
         figsize=(FIGWIDTH * len(meshes), FIGHEIGHT * len(coef_funcs)),
         squeeze=False,
-        sharex=True,
+        # sharex=True,
         sharey=True,
     )
 
@@ -76,9 +76,9 @@ def plot_sharpened_bound_vs_iterations(
                 array_zip = np.load(fp)
                 niters_sharp = array_zip["niters_sharp"]
                 niters_sharp_mixed = array_zip["niters_sharp_mixed"]
-                convergence_eigenvalues = array_zip[
-                    "eigenvalues"
-                ]  # eigenvalues at convergence
+
+                # eigenvalues at convergence
+                convergence_eigenvalues = array_zip["eigenvalues"]
             else:
                 approx_path = Path(__file__).parent / "sharpened_bound_vs_iterations.py"
                 LOGGER.error(
@@ -87,109 +87,148 @@ def plot_sharpened_bound_vs_iterations(
                 )
                 exit()
 
-            # plot the two versions of the sharpened bound
-            sharp_iters = niters_sharp
-            sharp_line = ax.plot(
-                range(len(sharp_iters)),
-                sharp_iters,
-                marker="v" if plot_bounds else None,
-                linestyle="None",
-                markersize=3,
+            plot_bounds(
+                ax,
+                shorthand,
+                convergence_eigenvalues,
+                niters_sharp,
+                niters_sharp_mixed,
+                show_bounds,
             )
-
-            sharp_mixed_iters = niters_sharp_mixed
-            sharp_mixed_line = ax.plot(
-                range(len(sharp_mixed_iters)),
-                sharp_mixed_iters,
-                marker="^" if plot_bounds else None,
-                linestyle="None",
-                markersize=3,
-            )
-
-            # plot moving average of sharpened bounds
-            sharp_bound_avg = np.convolve(
-                sharp_iters,
-                np.ones(MOVING_AVG_WINDOW) / MOVING_AVG_WINDOW,
-                mode="valid",
-            )
-            ax.plot(
-                MOVING_AVG_WINDOW + np.arange(len(sharp_bound_avg)),
-                sharp_bound_avg,
-                label=f"{shorthand} (MA)",
-                linestyle="--",
-                color="blue",
-            )
-
-            # plot moving min of sharp bound
-            moving_min = np.min(
-                np.lib.stride_tricks.sliding_window_view(
-                    sharp_iters, MOVING_AVG_WINDOW
-                ),
-                axis=1,
-            )
-            ax.plot(
-                MOVING_AVG_WINDOW + np.arange(len(moving_min)),
-                moving_min,
-                label=f"Sharpened bound (MM)",
-                linestyle="--",
-                color=sharp_line[0].get_color(),
-            )
-
-            # plot moving min of sharp mixed bound
-            moving_min = np.min(
-                np.lib.stride_tricks.sliding_window_view(
-                    sharp_mixed_iters, MOVING_AVG_WINDOW
-                ),
-                axis=1,
-            )
-            ax.plot(
-                MOVING_AVG_WINDOW + np.arange(len(moving_min)),
-                moving_min,
-                label=f"Sharpened Mixed Bound (MM)",
-                linestyle="--",
-                color=sharp_mixed_line[0].get_color(),
-            )
-
-            # plot upper bound at convergence
-            ax.axhline(
-                mixed_sharpened_cg_iteration_bound(
-                    convergence_eigenvalues, log_rtol=LOG_RTOL, exact_convergence=False
-                ),
-                linestyle="--",
-                color=ax.lines[-1].get_color(),
-                linewidth=0.8,
-            )
-
-            # plot actual number of iterations
-            n_iters = min(len(convergence_eigenvalues), N_ITERATIONS)
-            ax.axhline(
-                n_iters,
-                linestyle="-",
-                color=ax.lines[-1].get_color(),
-                linewidth=0.8,
-                label=f"{shorthand} (actual)",
-            )
-
-            # plot y=x
-            ax.plot(
-                np.arange(n_iters),
-                np.arange(n_iters),
-                linestyle="--",
-                color="black",
-                linewidth=0.8,
-                label="y=x",
-            )
-
-            # log scale y-axis
-            ax.set_yscale("log")
-
-            # grid settings
-            ax.grid(axis="x", which="both", linestyle="--", linewidth=0.7)
-            ax.grid(axis="y", which="both", linestyle=":", linewidth=0.5)
 
         # advance main task
         progress.advance(main_task)
 
+    style_figure(fig, axs, shorthand, meshes, coef_funcs)
+
+    # stop progress bar
+    progress.soft_stop()
+
+    return fig, shorthand
+
+
+def plot_bounds(
+    ax,
+    shorthand,
+    convergence_eigenvalues,
+    niters_sharp,
+    niters_sharp_mixed,
+    show_bounds: bool,
+):
+    # get number of iterations to plot
+    n_iters_plot = min(len(convergence_eigenvalues) - 1, N_ITERATIONS)
+
+    # plot the two versions of the sharpened bound
+    sharp_iters = niters_sharp
+    sharp_line = ax.plot(
+        range(n_iters_plot),
+        sharp_iters[:n_iters_plot],
+        marker="v" if show_bounds else None,
+        linestyle="None",
+        markersize=3,
+    )
+
+    sharp_mixed_iters = niters_sharp_mixed
+    sharp_mixed_line = ax.plot(
+        range(n_iters_plot),
+        sharp_mixed_iters[:n_iters_plot],
+        marker="^" if show_bounds else None,
+        linestyle="None",
+        markersize=3,
+    )
+
+    # # plot moving average of sharpened bounds
+    # sharp_bound_avg = np.convolve(
+    #     sharp_iters[:n_iters_plot],
+    #     np.ones(MOVING_AVG_WINDOW) / MOVING_AVG_WINDOW,
+    #     mode="same",
+    # )
+    # ax.plot(
+    #     MOVING_AVG_WINDOW + np.arange(len(sharp_bound_avg)),
+    #     sharp_bound_avg,
+    #     label=f"{shorthand} (MA)",
+    #     linestyle="--",
+    #     color="blue",
+    # )
+
+    # plot moving min of sharp bound
+    moving_min_sharp = np.min(
+        np.lib.stride_tricks.sliding_window_view(
+            sharp_iters[:n_iters_plot], MOVING_AVG_WINDOW
+        ),
+        axis=1,
+    )
+    ax.plot(
+        MOVING_AVG_WINDOW + np.arange(len(moving_min_sharp)),
+        moving_min_sharp,
+        label=f"Sharpened bound (MM)",
+        linestyle="--",
+        color=sharp_line[0].get_color(),
+    )
+
+    # plot moving min of sharp mixed bound
+    moving_min_sharp_mixed = np.min(
+        np.lib.stride_tricks.sliding_window_view(
+            sharp_mixed_iters[:n_iters_plot], MOVING_AVG_WINDOW
+        ),
+        axis=1,
+    )
+    ax.plot(
+        MOVING_AVG_WINDOW + np.arange(len(moving_min_sharp_mixed)),
+        moving_min_sharp_mixed,
+        label=f"Sharpened Mixed Bound (MM)",
+        linestyle="--",
+        color=sharp_mixed_line[0].get_color(),
+    )
+
+    # plot average of two moving mins
+    moving_mins_avg = (moving_min_sharp + moving_min_sharp_mixed) / 2
+    ax.plot(
+        MOVING_AVG_WINDOW + np.arange(len(moving_mins_avg)),
+        moving_mins_avg,
+        label=f"Sharpened Bound (MM Avg)",
+        linestyle="--",
+        color="black",
+    )
+
+    # plot upper bound at convergence
+    ax.axhline(
+        mixed_sharpened_cg_iteration_bound(
+            convergence_eigenvalues, log_rtol=LOG_RTOL, exact_convergence=False
+        ),
+        linestyle="--",
+        color=ax.lines[-1].get_color(),
+        linewidth=0.8,
+    )
+
+    # plot actual number of iterations
+    ax.axhline(
+        len(convergence_eigenvalues) - 1,
+        linestyle="-",
+        color=ax.lines[-1].get_color(),
+        linewidth=0.8,
+        label=f"{shorthand} (actual)",
+    )
+
+    # plot y=x
+    ax.plot(
+        np.arange(n_iters_plot),
+        np.arange(n_iters_plot),
+        linestyle="--",
+        color="black",
+        linewidth=0.8,
+        label="y=x",
+    )
+
+    # log scale y-axis
+    ax.set_yscale("log")
+
+    # grid settings
+    ax.grid(axis="x", which="both", linestyle="--", linewidth=0.7)
+    ax.grid(axis="y", which="both", linestyle=":", linewidth=0.5)
+
+
+def style_figure(fig, axs, shorthand, meshes, coef_funcs):
     # Add column titles (LaTeX, Nc as integer, no bold for compatibility)
     for col_idx, mesh_params in enumerate(meshes):
         H = mesh_params.coarse_mesh_size
@@ -225,16 +264,11 @@ def plot_sharpened_bound_vs_iterations(
     # tight layout for the figure
     fig.tight_layout(pad=1.3)
 
-    # stop progress bar
-    progress.soft_stop()
-
-    return fig, shorthand
-
 
 if ARGS.generate_output:
     for preconditioner in PRECONDITIONERS:
         fig, shorthand = plot_sharpened_bound_vs_iterations(
-            preconditioner, meshes=PLOT_MESHES, plot_bounds=PLOT_BOUNDS
+            preconditioner, meshes=PLOT_MESHES, show_bounds=SHOW_BOUNDS
         )
         fn = Path(__file__).name.replace("_fig.py", f"_{shorthand}")
         save_latex_figure(fn, fig)
@@ -242,7 +276,7 @@ if ARGS.show_output:
     figs = []
     for preconditioner in PRECONDITIONERS:
         fig, _ = plot_sharpened_bound_vs_iterations(
-            preconditioner, meshes=PLOT_MESHES, plot_bounds=PLOT_BOUNDS
+            preconditioner, meshes=PLOT_MESHES, show_bounds=SHOW_BOUNDS
         )
         figs.append(fig)
     plt.show()
